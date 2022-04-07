9d054fd27ccd4e078fbff45fda1301de19ba3011
jintoshaji <93276371+jintoshaji@users.noreply.github.com>
2022-04-06 23:25:21 -0700

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am A Advance Auto Filter Bot🤖 Exclusively Made For @ArchivedMovies ☑️
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@ArchivedMovies
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +43,15 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@ArchivedMovies
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/ArchivedMovies">ARCHIVED MOVIES</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
